ee2a10e23bcd34c238e8ff24ab2ef3ea763ac02e
Jean Costa <jean.adcc@gmail.com>
2022-02-08 16:30:25 -0300

    fix(back-end): implement typescript related fixes

--- packages/back-end/src/index.ts
+++ packages/back-end/src/index.ts
@@ -4,7 +4,7 @@
 import * as admin from "firebase-admin";
 if (admin.apps.length === 0) admin.initializeApp();
 
-import auth from "@/api/auth";
+import auth from "@api/auth";
 
+// eslint-disable-next-line import/prefer-default-export
 export { auth };
-export default { auth };
